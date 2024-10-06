c40c697276dd27200304f05b23c3cc2fb5a50e9f
Sudha1023 <123466529+Sudha1023@users.noreply.github.com>
2024-10-06 08:31:58 +0530

    Create Find_index.c

---
+++ Find_index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int find_index(int arr[], int n, int K)
+{
+	
+	for (int i = 0; i < n; i++)
+		if (arr[i] == K)
+			return i;
+		else if (arr[i] > K)
+			return i;
+	return n;
+}
+
+
+int main()
+{
+	int arr[] = { 1, 3, 5, 6 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int K = 2;
+	printf("%d\n", find_index(arr, n, K));
+	return 0;
+}
